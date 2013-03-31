c35b6a3ff46e2fc1ea5bcdfdf0b91bc74e4cd480
Aleksandr Rakov <aleksandr.rakov@gmail.com>
2013-03-31 22:51:49 +0400

    added tags widget

--- deform_select2/widget.py
+++ deform_select2/widget.py
@@ -1,7 +1,12 @@
-from deform.widget import SelectWidget
+from deform.widget import SelectWidget,TextInputWidget
 
 class Select2Widget(SelectWidget):
     template = 'select2'
     requirements = (('select2', None), )
     placeholder=''
     allowClear=False
+
+class Select2TagsTextWidget(TextInputWidget):
+    template = 'select2tagstext'
+    requirements = (('select2', None), )
+    tags=["red", "green", "blue"]
